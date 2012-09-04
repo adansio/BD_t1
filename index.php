a62c0dcaf6ef074498afbcbf3d4a17ae67c2f858
adansio <adansio@gmail.com>
2012-09-03 23:35:27 -0300

    v1 interfaz usuario

---
+++ index.php
@@ -0,0 +1,38 @@
+<html lang="es">
+	<header>
+		<title>
+			LibreriaUSM - T1 BD
+		</title>
+		<link rel="stylesheet" href="dis.css" type="text/css"/>
+	</header>
+	
+	<body>
+		<header class="cabecera">
+			<div class="titulo"><h2>Bienvenido a libreria USM</h2></div>
+		</header>
+		<div class="reg">
+			<ul class="registro">
+				<div class="tit_reg"><h3>Registrarse</h3></div>
+				<li>Rut: <input type="number" id="rut"/></li>
+				<li>nombres: <input type="search" id="name"/></li>
+				<li>apellidos: <input type="search" id="last_name"/></li>
+				<li>e-mail: <input type="email" name="mail"/></li>
+				<li>password: <input type="search" id="pass"/></li>
+			</ul>
+		</div>
+		<div class="Login">
+			<ul class="login_admin">
+				<li>Login Administrador</li>
+				<li>user: <input type="search" id="user" size="15"/></li>
+				<li>pass: <input type="search" id="passwd" size="15"/></li>
+			</ul>
+		</div>
+		<div class="buscar">
+			<div class="busq">Buscar <input type="search" id="buscar"/>
+		</div>
+		<footer>
+			<hr/>
+			Designed By bd22
+		</footer>
+	</body>
+</html>
